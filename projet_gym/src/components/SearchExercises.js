27f27488eaa540f1bffecc8b49ed5f42f4de10be
ouakarrouch <ouakarrouchmarouane10@gmail.com>
2023-06-22 20:49:48 +0200

    premier commit

--- projet_gym/src/components/SearchExercises.js
+++ projet_gym/src/components/SearchExercises.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 const SearchExercises = () => {
   return (
-    <div>
+    <Stack alignItems='center' mt='37px'
+    justifyContent='center'
+    p="20px">
+      <Typography  fontWeight={700}
+      sx={{
+        fontSize: {lg: '44px' , xs: '30px'}
+      }}
+      mb='50px' textAlign='center'
       
-    </div>
+      >
+
+    Awesome Exercises You <br /> 
+    Should Know 
+
+
+
+      </Typography>
+       <Box position='relative'
+       mb="72px">
+        <TextField
+sx={{
+input:{
+fontWeight:'700',
+border: 'none',
+borderRadius:'4px'
+},
+width: {lg: '800px', xs: '350px'},
+backgroundColor: '#fff',
+borderRadius: '40px'
+
+
+}}
+
+        height='76px'
+        value=""
+        onChange={(e) => {}}
+        placeholder='Search'
+        type="text"
+        
+        
+        />
+        <Button className='Search-btn'
+
+sx={{
+bgcolor:'#FF2625',
+color:'#fff',
+textTransform: 'none',
+width:{lg: '175px', xs: '80px'},
+fontSize:{lg:'20px', xs:'14px'},
+height:'56px',
+position: 'absolute'
+
+}}>Search</Button>
+
+
+
+
+
+
+       </Box>
+
+
+
+
+
+    </Stack>
   )
 }
 
